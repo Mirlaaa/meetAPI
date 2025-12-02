5c611da50f694f50c711c6078a234c12b971427c
Mirla Lopes <mirlaisabel11@gmail.com>
2025-12-01 23:19:45 -0300

    Start project

---
+++ src/main/java/br/com/amisahdev/meetAPI/MeetApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.amisahdev.meetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/amisahdev/meetAPI/MeetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.amisahdev.meetAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
